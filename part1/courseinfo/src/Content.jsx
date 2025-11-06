a980eda3e948842c771e8de8d359cf5ebf1de3e4
Vikram <vikram@vikramcodes.com>
2025-11-05 19:18:40 -0500

    changed folder structure

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,15 @@
+import Part from "./Part";
+const Content = (props) => 
+{
+    return(
+        <div>
+            <p>{props.part1} {props.exercises1}</p>
+            <p>{props.part2} {props.exercises2}</p>
+            <p>{props.part3} {props.exercises3}</p>
+            <Part />
+        </div>
+    )
+
+};
+
+export default Content;
---
+++ part1/courseinfo/src/Part.jsx
@@ -0,0 +1,11 @@
+const Part = (props) =>
+{
+    return
+    (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Part;
