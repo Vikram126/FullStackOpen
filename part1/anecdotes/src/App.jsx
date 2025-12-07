abf133b062bf176d9b60048c4c1b75785c473363
Vikram <vikram@vikramcodes.com>
2025-12-07 01:24:48 -0500

    anecdote

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+const Button = ({text,onClick}) => (<button onClick={onClick}>{text}</button> );
+
+const MostVotes = ({max,anecdote}) =>
+{
+  
+  if(max==0)
+  {
+    return(
+      <div>
+      <h1>Anecdote with most votes</h1>
+      No votes in yet.
+      </div>
+    )
+  }
+
+  return(
+    <div>
+      <h1>Anecdote with most votes</h1>
+      {anecdote} <br />
+      has {max} votes.
+
+    </div>
+  )
+}
+
+
+const App = () => {
+
+const randomSelection = () =>{
+  setSelected(Math.floor(Math.random()*8))
+}
+
+const incVote = () =>{
+  const copy = [...votes];
+  copy[selected]+=1
+  setVote(copy)
+}
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes,setVote] = useState(Array(anecdotes.length).fill(0))
+
+  const maxVotes = Math.max(...votes)
+  const indexMax = votes.indexOf(maxVotes)
+ 
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]} <br />
+      has {votes[selected]} votes. <br />
+      <Button text="vote" onClick={incVote}/>
+      <Button text="next anecdote" onClick={randomSelection}/>
+      <MostVotes max={maxVotes} anecdote={anecdotes[indexMax]}/>
+    </div>
+  )
+}
+
+export default App
