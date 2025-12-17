752d3d1bc58dcac821162943dfe9926275643c9d
Vikram <vikram@vikramcodes.com>
2025-12-16 19:43:52 -0500

    2.11 json server and axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const SearchField = ({setShowall,setSearchName}) => {
 
@@ -66,16 +67,25 @@ const PersonForm = ({handleSubmit,newName,newNumber,setNewName,setNewnumber}) =>
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewnumber] = useState('');
   const [searchName,setSearchName] = useState('');
   const [showall, setShowall] = useState(true);
+
+  const hook = () =>{
+    console.log('fetching data from the server')
+    axios.get('http://localhost:3001/persons')
+    .then(promise => {
+      console.log(promise.data)
+      setPersons(promise.data)
+    }
+
+    )
+  }
+
+  useEffect(hook,[]);
 
 
   const handleSubmit = (event) => {
